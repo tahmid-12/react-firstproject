43b7e137828393f1add5076d26b7ea60a2585c3c
Md. Tahmid CHowdhury <mdtahmidchowdhury12@gmail.com>
2020-11-24 09:26:57 +0600

    1st React Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.json";
+import Products from "./Components/Products";
+import Filter from "./Components/Filter";
+import Cart from "./Components/Cart";
+//import store from "./store";
+//import { Provider } from "react-redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      cartItems: localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")):[],
+      size: "",
+      sort: "",
+    }
+  }
+  createOrder = (order) => {
+    alert("Need to save for " + order.name);
+  }
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({cartItems: 
+      cartItems.filter(x=>x._id !== product._id)
+    });
+    localStorage.setItem("cartItems",
+                          JSON.stringify(cartItems.filter(x=>x._id !== product._id)));
+  }
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach(item => {
+      if(item._id === product._id){
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if(!alreadyInCart){
+      cartItems.push({...product, count: 1})
+    }
+    this.setState({cartItems});
+    localStorage.setItem("cartItems",JSON.stringify(cartItems));
+  }
+
+  sortProducts = (event) => {
+
+    const sort = event.target.value;
+
+    this.setState(state => ({
+      sort: sort,
+      products: this.state.products.slice().sort((a,b) => (
+        sort === "lowest"?
+        ((a.price > b.price)? 1:-1):
+        sort === "highest"?
+        ((a.price < b.price)? 1:-1):
+        ((a._id < b._id)? 1:-1)
+      ))
+    }))
+  }
+
+  filterProducts = (event) => {
+
+    if(event.target.value === ""){
+      this.setState({ size: event.target.value, product: data.products });
+    }else{
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(product => product.availableSizes.indexOf(event.target.value)>=0
+        ),
+      })
+    }
+    
+  }
+  render(){
+    return (
+      <div className="grid-container">
+  
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+  
+        <main>
+          <div className="content">
+             <div className="main">
+               <Filter count={this.state.products.length}
+                       size={this.state.size}
+                       sort={this.state.sort}
+                       filterProducts={this.filterProducts}
+                       sortProducts={this.sortProducts}/>
+               <Products 
+                  products={this.state.products} 
+                  addToCart={this.addToCart}/> 
+             </div>
+             <div className="sidebar">
+               <Cart cartItems={this.state.cartItems} 
+                     removeFromCart={this.removeFromCart}
+                     createOrder={this.createOrder}/>
+             </div>
+          </div>
+        </main>
+  
+        <footer>
+          All rights are reserved
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Products.js
@@ -0,0 +1,121 @@
+import React, { Component } from 'react'
+import formatCurrency from '../util';
+import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal';
+import Zoom from 'react-reveal/Zoom';
+//import { connect } from "react-redux";
+//import { fetchProducts } from "../actions/productActions";
+
+export default class Products extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            product: null,
+        };
+    }
+    /*
+    componentDidMount(){
+        this.props.fetchProducts();
+    }
+    */
+    openModal = (product) =>{
+        this.setState({ product });
+    };
+    closeModal = () => {
+        this.setState({ product:null }); 
+    };
+    render() {
+        const { product } = this.state;
+        return (
+            <div>
+              <Fade bottom cascade={true}>
+                  {
+                      !this.props.products ? <div>Loading..</div> : 
+                      (
+                      <ul className="products">
+                        {this.props.products.map(product => (
+                         <li key={product._id}>
+
+                            <div className="product">
+
+                                <a href={"#" + product._id} onClick={() => this.openModal(product)}>
+                                    <img src={product.image} alt={product.title}/>
+                                    <p>
+                                        {product.title}
+                                    </p>
+                                </a>
+
+                                <div className="product-price">
+
+                                    <div>
+                                       {formatCurrency(product.price)}
+                                    </div>
+
+                                    <button onClick={() => this.props.addToCart(product)} className="button primary">
+                                        Add to Cart
+                                    </button>
+                                </div>
+                            </div>
+
+                         </li>
+                    ))}
+                </ul>
+                )
+                  }
+                
+             </Fade>
+             {
+                 product && (
+                     <Modal isOpen={true} onRequestClose={this.closeModal}>
+                         <Zoom>
+                            <button className="close-modal" onClick={this.closeModal}>x</button>
+                            <div className="product-details">
+                                <img src={product.image} alt={product.title}/>
+                                <div className="product-details-description">
+                                    <p>
+                                        <string>{product.title}</string>
+                                    </p>
+                                    <p>
+                                        <string>{product.description}</string>
+                                    </p>
+                                    <p>
+                                        Available Sizes:{" "}
+                                        {product.availableSizes.map(x => (
+                                            <span> 
+                                                {" "}
+                                                <button className="button">
+                                                    {x}
+                                                </button> 
+                                            </span>
+                                        ) )}
+                                    </p>
+                                    <div className="product-price">
+                                        <div>
+                                            {formatCurrency(product.price)}
+                                        </div>
+                                        <button className="button primary"
+                                                onClick={() => {
+                                                    this.props.addToCart(product); 
+                                                    this.closeModal();
+                                                }}>
+                                                    Add to Cart
+                                                </button>
+                                    </div>
+                                </div>
+                            </div>     
+                         </Zoom>
+                     </Modal>
+                 )
+             }
+            </div>
+        )
+    }
+}
+
+/*
+export default connect(
+    (state)=> ({products: state.products.items}),
+    {
+        fetchProducts,
+    })(Products);
+*/
